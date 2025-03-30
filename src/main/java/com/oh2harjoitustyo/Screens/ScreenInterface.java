26ded2244d84e6e1b2bdffc6b87a4eadff9756f0
ananpn <anttpw@gmail.com>
2025-03-30 02:25:24 +0200

    Added comments, modifications and fixes

---
+++ src/main/java/com/oh2harjoitustyo/Screens/ScreenInterface.java
@@ -0,0 +1,10 @@
+package com.oh2harjoitustyo.Screens;
+
+import com.oh2harjoitustyo.SceneManager;
+import javafx.scene.layout.Pane;
+
+interface ScreenInterface {
+    Pane getScreen();
+
+    void createScreen();
+}
